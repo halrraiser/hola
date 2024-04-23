6b3af680f24449eca20810fbcca6376e18be50f7
halrraiser <147226841+halrraiser@users.noreply.github.com>
2024-04-24 00:20:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["$help","+300 Servers😈","skript.gg"];
+const statusMessages = ["&help","NOT RAID BOT","skript.gg"];
 
 
 let currentIndex = 0;
